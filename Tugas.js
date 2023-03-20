1279f1722edac7b4e0947ad8a811bb7099419b64
Asepsa15 <asepsaripudin2002@gmail.com>
2023-03-20 21:46:12 +0700

    Tugas

---
+++ Tugas.js
@@ -0,0 +1,11 @@
+for (let nilai = 1; nilai <= 10000; nilai++) {
+  if (nilai % 3 == 0 && nilai % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (nilai % 3 == 0) {
+    console.log("Fizz");
+  } else if (nilai % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(nilai);
+  }
+}
